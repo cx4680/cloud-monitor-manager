a7561c727c93d8a6ba9feb96de2dafdd2e819895
陈行 <chenxing@cestc.cn>
2022-05-18 14:44:40 +0800

    修改ecs实例列表返回字段

--- external/ecs_instance.go
+++ external/ecs_instance.go
@@ -106,7 +106,7 @@ func (ecs *EcsInstanceService) ConvertResp(realResp interface{}) (int, []service
 					Name:  "status",
 					Value: d.StatusDesc,
 				}, {
-					Name:  "systemType",
+					Name:  "osType",
 					Value: ecsAdditional.SystemType,
 				}},
 			})
