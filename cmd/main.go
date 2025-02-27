22af0fd77b863084fdc936783566593ff0f27456
chenxing <468046048@QQ.com>
2025-02-27 17:54:13 +0800

    update：移除无法使用的组件和依赖

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"code.cestc.cn/ccos-ops/cloud-monitor-manager/config"
 	"code.cestc.cn/ccos-ops/cloud-monitor-manager/global/pipeline"
-	"code.cestc.cn/ccos-ops/cloud-monitor-manager/global/sys_component/sys_db"
-	"code.cestc.cn/ccos-ops/cloud-monitor-manager/global/sys_component/sys_redis"
-	"code.cestc.cn/ccos-ops/cloud-monitor-manager/global/task"
-	"code.cestc.cn/ccos-ops/cloud-monitor-manager/logger"
 	"code.cestc.cn/ccos-ops/cloud-monitor-manager/validator/translate"
 	"code.cestc.cn/ccos-ops/cloud-monitor-manager/web"
 	"context"
@@ -17,40 +13,40 @@ import (
 func main() {
 	loader := pipeline.NewMainLoader()
 
-	loader.AddStage(func(*context.Context) error {
-		if err := sys_db.InitDb(config.Cfg.Db); err != nil {
-			logger.Logger().Errorf("init database error: %v\n", err)
-			return err
-		}
-		return nil
-	})
+	//loader.AddStage(func(*context.Context) error {
+	//	if err := sys_db.InitDb(config.Cfg.Db); err != nil {
+	//		logger.Logger().Errorf("init database error: %v\n", err)
+	//		return err
+	//	}
+	//	return nil
+	//})
 
-	loader.AddStage(func(*context.Context) error {
-		if err := sys_redis.InitClient(config.Cfg.Redis); err != nil {
-			logger.Logger().Errorf("init redis error: %v\n", err)
-			return err
-		}
-		return nil
-	})
+	//loader.AddStage(func(*context.Context) error {
+	//	if err := sys_redis.InitClient(config.Cfg.Redis); err != nil {
+	//		logger.Logger().Errorf("init redis error: %v\n", err)
+	//		return err
+	//	}
+	//	return nil
+	//})
 
-	loader.AddStage(func(*context.Context) error {
-		return sys_db.InitData(config.Cfg.Db, "hawkeye", "file://./migrations")
-	})
+	//loader.AddStage(func(*context.Context) error {
+	//	return sys_db.InitData(config.Cfg.Db, "hawkeye", "file://./migrations")
+	//})
 
 	loader.AddStage(func(*context.Context) error {
 		return translate.InitTrans("zh")
 	})
 
-	loader.AddStage(func(*context.Context) error {
-		bt := task.NewBusinessTaskImpl()
-		for _, v := range task.BusinessTaskList {
-			if err := bt.Add(v); err != nil {
-				return err
-			}
-		}
-		bt.Start()
-		return nil
-	})
+	//loader.AddStage(func(*context.Context) error {
+	//	bt := task.NewBusinessTaskImpl()
+	//	for _, v := range task.BusinessTaskList {
+	//		if err := bt.Add(v); err != nil {
+	//			return err
+	//		}
+	//	}
+	//	bt.Start()
+	//	return nil
+	//})
 
 	loader.AddStage(func(*context.Context) error {
 		return web.Start(config.Cfg.Serve)
